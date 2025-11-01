d8fb835127ba3804d884364d2816f8b997b00558
Anandi11 <anandiraghavi2005@gmail.com>
2025-11-01 12:49:14 +0530

    Update ExpenseRepository.java

--- backend/src/main/java/com/tesseract/financeapp/repository/ExpenseRepository.java
+++ backend/src/main/java/com/tesseract/financeapp/repository/ExpenseRepository.java
@@ -5,7 +5,5 @@ import com.tesseract.financeapp.model.Expense;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-
-    // Correct way to query by nested property 'user.id'
     List<Expense> findByUser_Id(Long userId);
 }
